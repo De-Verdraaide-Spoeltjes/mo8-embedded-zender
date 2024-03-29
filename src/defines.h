4eb6acde33c968c2795c52c483c453bc9fa73bf8
Jochem <jochem323@proton.me>
2024-03-29 16:27:41 +0100

    Fix rsa data struct

--- src/defines.h
+++ src/defines.h
@@ -1,10 +1,12 @@
 #ifndef SRC_DEFINES_H
 #define SRC_DEFINES_H
 
+#include <stdint.h>
+
 typedef struct RSAData {
 	uint64_t publicKey;
 	uint64_t privateKey;
 	uint64_t modulus;
-};
+} rsaData;
 
 #endif
