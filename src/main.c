012119bf8173ea4ba731835f78c49cb6a31f37b9
Jochem <jochem323@proton.me>
2024-06-10 10:18:58 +0200

    Status stuff & test RSA keys

--- src/main.c
+++ src/main.c
@@ -6,16 +6,32 @@
 #include "xil_printf.h"
 #include "xscugic.h"
 #include "xgpio.h"
+#include "xtime_l.h"
 
 #include "interrupt_controller.h"
 #include "rsa_encryption.h"
 #include "rsa_key_receiver.h"
 
+#define STATUS_LED_DEVICE_ID    XPAR_CONNECTION_EMBEDDED_STATUS_LED_DEVICE_ID
+XGpio leds;
+
 XScuGic InterruptController;
 rsaData RSAData;
 
+void statusLED();
+
 int main() {
     init_platform();
+
+    XGpio_Initialize(&leds, STATUS_LED_DEVICE_ID);
+	XGpio_SetDataDirection(&leds, 1, 0x0);
+    XGpio_DiscreteWrite(&leds, 1, 0x3);
+
+    #ifdef DEBUG
+        RSAData.publicKey = 0x03;
+        RSAData.modulus = 0xF23A877;
+    #endif
+
     print("Starting embedded application\n\r");
 
     XStatus status;
@@ -57,11 +73,24 @@ int main() {
         return 0;
     }
 
-//    TODO: Add status LED
+    XGpio_DiscreteWrite(&leds, 1, 0x2);
 
     print("Embedded application initialized\n\r");
 
-    while (1);
+    while (1) statusLED();
     cleanup_platform();
     return 0;
+}
+
+void statusLED() {
+    XTime tNow;
+    static XTime tOld;
+	static bool state;
+    XTime_GetTime(&tNow);
+    if (tOld + 65000000 < tNow) {
+        // Toggle status LED
+        XGpio_DiscreteWrite(&leds, 1, state << 2);
+        state = !state;
+        tOld = tNow;
+    }
 }
